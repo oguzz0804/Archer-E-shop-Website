929f0763cd593761eb7d7e58eab4aa44ef610398
Berkay Alatas <berkay.3304@gmail.com>
2021-11-22 22:52:19 +0300

    added env variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/Nav/NavBar";
 import { IndexPresentation } from "./components/Home/IndexPresentation";
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import "@firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyA8JzwCpLnWxlHPpmuVR437_KE7b_W9TWo",
-  authDomain: "archerdb-4601b.firebaseapp.com",
-  databaseURL: "https://archerdb-4601b-default-rtdb.firebaseio.com/",
-  projectId: "archerdb-4601b",
-  storageBucket: "archerdb-4601b.appspot.com",
-  messagingSenderId: "112012461960",
-  appId: "1:112012461960:web:f3526c24a58ecde26d5c3d"
+  apiKey: process.env.API_KEY,
+  authDomain:process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket:process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 });
 
 export function getFirebase() {
